069f820ee094205182fba970178e447f9daf9b1d
iagop <iago.ponte2@gmail.com>
2025-03-03 11:44:06 -0300

    index fix - tentativa

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
 
-router.use("/api", require("./products"))
+router.use("/api", require("./products/productRoutes"))
 
 module.exports = router
