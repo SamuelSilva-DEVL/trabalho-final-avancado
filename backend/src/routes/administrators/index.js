3fc17cd2ed66ac26e44187cb1fce340ed1735103
iagop <iago.ponte2@gmail.com>
2025-03-17 13:44:47 -0300

    fix: without jwt

--- backend/src/routes/administrators/index.js
+++ backend/src/routes/administrators/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const adminController = require('../../controllers/adminController')
 
-router.post("/admin/login", adminController.userLogin);
+// router.post("/admin/login", adminController.userLogin);
 
 router.get("/admin", adminController.getUsers);
 router.get("/admin/:id", adminController.getUserById);
